2f12357556259195f8652699359110f3e23947af
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-02-11 18:01:29 +0330

    IMP => test

--- task_manager/task/tests.py
+++ task_manager/task/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import Task
+from django.core.exceptions import ValidationError
 # TODO: Test for create save and update methods
 # Create your tests here.
+
+class TestTask(TestCase):
+    def test_create(self):
+        test_model = Task.objects.create(
+            title="pass"
+        )
+
+        self.assertEqual(
+            test_model.priority,
+            Task.PRIORITY_CHOICES.LOW
+        )
+        self.assertEqual(
+            test_model.state,
+            Task.STATE_CHOICES.TODO
+        )
+        try:
+            Task.objects.create()
+            assert False, \
+                "must raise ValidationError with saving Task with no title"
+            
+        except ValidationError as e:
+            pass
+        
+            

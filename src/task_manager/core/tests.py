1c09d3b7b1c5030c1f4b40254c67142ad3f4f402
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-03-20 11:40:25 +0330

    IMP => some tests

--- src/task_manager/core/tests.py
+++ src/task_manager/core/tests.py
@@ -1,27 +1,25 @@
 from django.test import TestCase
 from .models import User
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import Group
+
 
 class TestUser(TestCase):
-    def test_create(self):
-        test_model = User.objects.create(
-            username="pass",
-            email="pass@pass.com",
-            password="pass"
-        )
-        self.assertEqual(
-                test_model.username,
-                "pass"
-            )
-        self.assertEqual(
-                test_model.email,
-                "pass@pass.com"
-            )
-        try:
-            User.objects.create()
-            assert False, \
-                "must raise ValidationError with saving User with no username"
+    def setUp(self) -> None:
+        Group.objects.create(name="task_users")
 
-        except ValidationError as e:
+        return super().setUp()
+
+    def test_create(self):
+        test_model = User.objects.create_user(
+            username="pass", email="pass@pass.com", password="pass"
+        )
+        self.assertEqual(test_model.username, "pass")
+        self.assertEqual(test_model.email, "pass@pass.com")
+        assert test_model.groups.filter(name="task_users").exists()
+        try:
+            User.objects.create_user()
+            assert False, "must raise ValidationError with saving User with no username"
+
+        except TypeError as e:
             pass
-        

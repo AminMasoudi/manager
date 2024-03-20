381cd670fbf39a5827386d519479d6e7005ff7fd
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-03-20 10:56:08 +0330

    UPD => ...

--- src/task_manager/core/models.py
+++ src/task_manager/core/models.py
@@ -1,23 +1,17 @@
 from typing import Iterable
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.contrib.contenttypes.models import ContentType
-from task.models import Task
 # Create your models here.
 
 
 class User(AbstractUser):
     def save(self, *args, **kwargs) -> None:
-        base_user_group = Group.objects.filter(name="Base_Users").all()
-        print(base_user_group)
-        if  len(base_user_group)==0:
-            base_user_group = Group.objects.create(name="Base_Users")
-            base_user_group.permissions.set(self.perm())
+        task_user_group = Group.objects.filter(name="task_users").first()
+        # assign task_user group to user
+        if task_user_group is None:
+            raise ValueError("Task_user group is not found run init_task_app command")
+        super().save(*args, **kwargs)
+        self.groups.add(task_user_group)
         self.is_staff = True
         return super().save(*args, **kwargs)
 
-    def perm(self):
-        content_type = ContentType.objects.get_for_model(Task)
-        permission = Permission.objects.filter(
-            content_type=content_type,
-        )
-        return permission

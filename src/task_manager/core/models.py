d7888235e9f6c4c5c20041a610e4af679c939c02
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-03-20 11:40:02 +0330

    UPD  =>

--- src/task_manager/core/models.py
+++ src/task_manager/core/models.py
@@ -1,17 +1,21 @@
 from typing import Iterable
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import UserManager
+
 # Create your models here.
 
 
 class User(AbstractUser):
+
     def save(self, *args, **kwargs) -> None:
         task_user_group = Group.objects.filter(name="task_users").first()
         # assign task_user group to user
         if task_user_group is None:
-            raise ValueError("Task_user group is not found run init_task_app command")
-        super().save(*args, **kwargs)
+            raise ValueError("task_users group is not found. run init_task_app command")
+        r = super().save(*args, **kwargs)
         self.groups.add(task_user_group)
         self.is_staff = True
-        return super().save(*args, **kwargs)
+        return r
 
+    objects = UserManager()
